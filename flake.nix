c616091b3da54230f5cdac7525fafa52b4897296
Axel <nigthmaresans2@gmail.com>
2025-10-18 14:49:10 -0600

    removed dbus python

--- flake.nix
+++ flake.nix
@@ -1,47 +1,53 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    python3 = (pkgs.python313.withPackages (ps: with ps; [
-      pygobject3
-      dbus-python
-      opencv4
-      numpy
-    ]));
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      python3 = (
+        pkgs.python313.withPackages (
+          ps: with ps; [
+            pygobject3
+            opencv4
+            numpy
+          ]
+        )
+      );
 
-    nativeBuildInputs = with pkgs; [
-      python3
-      meson
-      ninja
-      pkg-config
-      wrapGAppsHook4
-      gobject-introspection
-    ];
-    buildInputs = with pkgs; [
-      glib
-      gtk4
-      libadwaita
-    ];
-  in {
-    
-    devShells.${system}.default = pkgs.mkShell {
-      inherit buildInputs;
-      nativeBuildInputs = nativeBuildInputs ++ [
-        python3.pkgs.pygobject-stubs
+      nativeBuildInputs = with pkgs; [
+        python3
+        meson
+        ninja
+        pkg-config
+        wrapGAppsHook4
+        gobject-introspection
       ];
-      packages = with pkgs; [
-        d-spy
+      buildInputs = with pkgs; [
+        glib
+        gtk4
+        libadwaita
       ];
-    };
+    in
+    {
 
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      name = "dbusqrdecoder";
-      versin = "0.1";
-      src = ./.;
+      devShells.${system}.default = pkgs.mkShell {
+        inherit buildInputs;
+        nativeBuildInputs = nativeBuildInputs ++ [
+          python3.pkgs.pygobject-stubs
+        ];
+        packages = with pkgs; [
+          d-spy
+        ];
+      };
 
-      inherit nativeBuildInputs buildInputs;
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "dbusqrdecoder";
+        versin = "0.1";
+        src = ./.;
+
+        inherit nativeBuildInputs buildInputs;
+      };
     };
-  };
 }
